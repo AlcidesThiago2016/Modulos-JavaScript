8d3d6aed17d8a02efdb908dd5ed5855dc80bcb24
alcid <alcides_thiago@hotmail.com>
2022-07-18 16:20:14 -0300

    First Commit

---
+++ main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraHobby, mostraIdade } from "./funcoes.mjs";
+
+console.log(mostraIdade("Alcides", 35));
+console.log(mostraCidade("Alcides", "Brasilia - DF"));
+console.log(mostraHobby("Alcides", "Programar"));
